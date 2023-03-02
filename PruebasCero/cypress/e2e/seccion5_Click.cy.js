b7311d9827ed70e4ef7000680858d7029337b220
Yudisel Santana Pacheco <yudisel@gmail.com>
2023-03-02 22:16:00 +0100

    Create seccion5_Click.cy.js
    intro asserts and click simple

---
+++ PruebasCero/cypress/e2e/seccion5_Click.cy.js
@@ -0,0 +1,18 @@
+///<reference types="Cypress"/>
+
+describe('Opciones Click', () => {
+    it('Introduccion Asserts', () => {
+        cy.visit('https://demoqa.com/text-box')
+        cy.get("#username").should('be.visible').type('Yudisel')      //be.visible, not be.visible, be.enable
+        cy.wait(2000)
+    })
+  
+    it('Click Sencillo', () => {
+      cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+      cy.get('eq','OrangeHRM')
+      cy.get('#username').type('Admin')
+      cy.get('#password').type('admin123')
+      cy.get('#submit').click
+    })
+  
+  })

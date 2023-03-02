c01828519d0ad210068fd646c77a317ff641ae15
Yudisel Santana Pacheco <yudisel@gmail.com>
2023-03-02 11:31:35 +0100

    funcion type
    page up
    page down

--- PruebasCero/cypress/e2e/seccion5.cy.js
+++ PruebasCero/cypress/e2e/seccion5.cy.js
@@ -5,9 +5,21 @@ describe('Seccion 5, pruebas con cypress', () => {
   it('Validando el título de la pagina', () => {
     cy.visit('https://demoqa.com/text-box')
     cy.title().should('eq','DEMOQA')              //'eq' ASSERT para comparar stricto DOS string
-
     cy.log("La funcion title esta OK")    
   })
 
+  it('Funcion type --enter', () => {           //Abrir google, escribir la busqueda y dar enter para comenzar
+    cy.visit('https://www.google.pl/')
+    cy.get("['name = 'q']").type('cypress{enter}')
+    cy.wait(3000)
+    cy.get("#rso > div:nth-child(1) > div > div > div > div > div > div > div > div.yuRUbf > a > h3").click()    
+  })
+  it('Funcion tye --pageUp --pageDown', () => {
+    cy.visit('https://demoqa.com/text-box')
+    cy.get("#username").type('{pageup}')       //Subir y bajar la page.
+    cy.wait(2000)
+    cy.get("#username").type('{pagedown}')
+       
+  })
 
 })
--- PruebasCero/cypress/e2e/spec.cy.js
+++ PruebasCero/cypress/e2e/spec.cy.js
@@ -1,5 +1,12 @@
+///<reference types="Cypress"/>
+
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+
+  it('probando la red', () => {
+    cy.log('hola mundo')
+  })
+
 })
